9be78d40f04de338e508c50c2561c52576e27b51
Kasia <katarzynajung@outlook.com>
2018-02-15 20:00:35 +0100

    move project to github

---
+++ js/filter.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(".button").click(function(){
+    var value = $(this).attr("data-filter");
+    if(value == "all"){
+      $(".filter").show("1000");
+    }
+    else
+    {
+      $(".filter").not("."+ value).hide("1000");
+      $(".filter").filter("."+ value).show("1000");
+    }
+    //add active class
+    $("ul .button").click(function(){
+      $(this).addClass('active');
+    })
+  });
+});
